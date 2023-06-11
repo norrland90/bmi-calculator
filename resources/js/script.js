1c6623ba5f774564c58cc2b5d9990328550ff26b
Norrland <79220667+norrland90@users.noreply.github.com>
2023-06-11 18:55:00 +0200

    add submit button to form

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,3 +16,18 @@ function hideMetric() {
 
 inputMetric.addEventListener('input', hideImperial);
 inputImperial.addEventListener('input', hideMetric);
+
+// Test submit
+
+form = document.querySelector('.form');
+
+function testFunc(e) {
+  e.preventDefault();
+  // console.log(e.target[0].value, inputMetric.value);
+  // Get checked value
+  console.log(document.querySelector('.form__radio-btn:checked').value);
+}
+
+form.addEventListener('submit', testFunc);
+
+// Möjligen finns olika sätt att lösa det med submit-grejen. Har dels gjort en submit-knapp som gör validering och beräkning när man trycker. Men möjligen istället gör så att beräkningen sker "automatiskt" när man trycker in siffrorna - i designen finns ingen submit-knapp.
